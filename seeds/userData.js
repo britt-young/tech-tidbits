2970651dcf12829cced903240efb11c573bbfccf
Britt Young <140761880+britt-young@users.noreply.github.com>
2024-02-01 13:56:24 -0500

    add
    user seed data

---
+++ seeds/userData.js
@@ -0,0 +1,18 @@
+// Initial user seed data
+[
+    {
+      "name": "Britt",
+      "email": "Britt@Young.com",
+      "password": "BY123456"
+    },
+    {
+      "name": "YoungMoney",
+      "email": "Dolla@Bills.com",
+      "password": "DollaDollaBill$"
+    },
+    {
+      "name": "YoungBl00d",
+      "email": "Young@Bl00d.com",
+      "password": "YoungBl00d1234"
+    }
+]
