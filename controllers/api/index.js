4bec98fa429473ee52bf571a906ceb253f9af3f2
Britt Young <140761880+britt-young@users.noreply.github.com>
2024-02-01 14:07:37 -0500

    add
    user requests js

---
+++ controllers/api/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router()
+const userRoutes = require('./userRoute')
+const blogRoutes = require('./blogRoute')
+const commentRoutes = require('./commentRoute')
+
+router.use('/users', userRoutes)
+router.use('/blogs', blogRoutes)
+router.use('/comments', commentRoutes)
+
+module.exports = router
