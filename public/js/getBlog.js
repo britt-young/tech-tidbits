eee90143e751a579241bfdd7a5248e657b3f46f6
Britt Young <140761880+britt-young@users.noreply.github.com>
2024-02-01 13:48:12 -0500

    add
    linked js to handlebars files

--- public/js/getBlog.js
+++ public/js/getBlog.js
@@ -1,6 +1,6 @@
 // Upon button click, user will be taken to the createblog page
 const getPost = document.getElementById('getPost')
 
-grabPost.addEventListener('click', (event) => {
+getPost.addEventListener('click', (event) => {
   window.location.href = '/createblog'
 })
