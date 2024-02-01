f90442b22719a079441efc281d6bab4c5e24c3a0
Britt Young <140761880+britt-young@users.noreply.github.com>
2024-02-01 11:16:09 -0500

    add
    middleware for login

---
+++ utlis/auth.js
@@ -0,0 +1,10 @@
+// Middleware authorization of login status before rendering the page
+const withAuth = (req, res, next) => {
+    if (!req.session.logged_in) {
+      res.redirect('/login');
+    } else {
+      next();
+    }
+  };
+  
+  module.exports = withAuth;
